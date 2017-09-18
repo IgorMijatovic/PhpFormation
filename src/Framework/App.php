95b5c8549fd06ee6802e7813b2454e541f3ea24f
igor <igor.mijatovic@outlook.de>
2017-09-18 22:54:52 +0200

    Course 2 Le Router

--- src/Framework/App.php
+++ src/Framework/App.php
@@ -7,10 +7,32 @@ use Psr\Http\Message\ServerRequestInterface;
 
 class App
 {
+    /**
+     * List of modules
+     * @var array
+     */
+    private $modules = [];
+
+    /**
+     * @var Router
+     */
+    private $router;
+
+    /**
+     * App constructor.
+     * @param string[] $modules Liste des modules a charger
+     */
+    public function __construct(array $modules = [])
+    {
+        $this->router = new Router();
+        foreach ($modules as $module) {
+            $this->modules[] = new $module($this->router);
+        }
+    }
+
     public function run(ServerRequestInterface $request): ResponseInterface
     {
         $uri = $request->getUri()->getPath();
-        var_dump($uri[-1]);
         if (!empty($uri) && $uri[-1] === "/") {
             $response = new Response();
             $response = $response->withStatus(301);
@@ -18,10 +40,21 @@ class App
 
             return $response;
         }
-        if ($uri == '/blog') {
-            return new Response(200, [], '<h1>Bienvenue sur le blog</h1>');
+        $route = $this->router->match($request);
+        if (is_null($route)) {
+            return new Response(404, [], '<h1>Error 404</h1>');
         }
-
-        return new Response(404, [], '<h1>Error 404</h1>');
+        $params = $route->getParams();
+        $request = array_reduce(array_keys($params), function ($request, $key) use ($params) {
+            return $request->withAttribute($key, $params[$key]);
+        }, $request);
+        $response = call_user_func_array($route->getCallback(), [$request]);
+        if (is_string($response)) {
+            return new Response(200, [], $response);
+        } elseif ($response instanceof ResponseInterface) {
+            return $response;
+        } else {
+            throw new \Exception('The response is not a sring or an instance of ResponseInterface');
+        }
     }
 }
---
+++ tests/Framework/RouterTest.php
@@ -0,0 +1,73 @@
+<?php
+namespace Tests\Framework;
+
+
+use Framework\Router;
+use GuzzleHttp\Psr7\ServerRequest;
+use PHPUnit\Framework\TestCase;
+
+class RouterTest extends TestCase
+{
+    /**
+     * @var Router
+     */
+    private $router;
+
+    public function setUp()
+    {
+        $this->router = new Router();
+    }
+
+    public function testGetMethod()
+    {
+        $request = new ServerRequest('get', '/blog');
+        $this->router->get('/blog', function () {
+            return 'hallo';
+        }, 'blog');
+        $route = $this->router->match($request);
+        $this->assertEquals('blog', $route->getName());
+        $this->assertEquals('hallo', call_user_func_array($route->getCallback(), [$request]));
+    }
+
+    public function testGetMethodIfRouteDoesnNotMatch()
+    {
+        $request = new ServerRequest('get', '/blog');
+        $this->router->get('/blogaze', function () {
+            return 'hallo';
+        }, 'blog');
+        $route = $this->router->match($request);
+        $this->assertEquals(null, $route);
+    }
+
+    public function testGetMethodWihParameters()
+    {
+        $request = new ServerRequest('get', '/blog/mon-slug-8');
+        $this->router->get('/blog', function () {
+            return 'azezea';
+        }, 'posts');
+        $this->router->get('/blog/{slug:[a-z0-9\-]+}-{id:\d+}', function () {
+            return 'hallo';
+        }, 'post.show');
+        $route = $this->router->match($request);
+        $this->assertEquals('post.show', $route->getName());
+        $this->assertEquals('hallo', call_user_func_array($route->getCallback(), [$request]));
+        $this->assertEquals(['slug' => 'mon-slug', 'id' => '8'], $route->getParams());
+
+        //Test invalid url
+        $route = $this->router->match(new ServerRequest('GET', '/blog/mon_slug-8'));
+        $this->assertEquals(null, $route);
+    }
+
+    public function testGenerateUri()
+    {
+        $this->router->get('/blog', function () {
+            return 'azezea';
+        }, 'posts');
+        $this->router->get('/blog/{slug:[a-z0-9\-]+}-{id:\d+}', function () {
+            return 'hallo';
+        }, 'post.show');
+        $uri =  $this->router->generateUri('post.show', ['slug' => 'mon-article', 'id' =>'8']);
+        $this->assertEquals('/blog/mon-article-8', $uri);
+    }
+
+}
